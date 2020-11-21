a4842ecdca8b9aad4d0c29d0b9eb9d9122f64bb1
Julio Machado <juliocarlos00@hotmail.com>
2020-11-21 18:17:21 -0300

    dinamic component import

---
+++ first-project-next/src/components/AddToCartModal.tsx
@@ -0,0 +1,9 @@
+export default function AddToCartModal() {
+    return (
+        <div>
+            <span>
+                Do you really want to add this product to the cart?
+            </span>
+        </div>
+    )
+}
