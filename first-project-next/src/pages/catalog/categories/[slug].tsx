fc51228067835b322f629b3d7ca87a8c02e926c1
Julio Machado <juliocarlos00@hotmail.com>
2020-11-22 17:24:59 -0300

    search page

--- first-project-next/src/pages/catalog/categories/[slug].tsx
+++ first-project-next/src/pages/catalog/categories/[slug].tsx
@@ -1,31 +1,40 @@
 import { useRouter } from 'next/router'
 
 import { GetStaticPaths, GetStaticProps } from "next"
-
-interface IProduct {
-    id: string;
-    title: string;
-}
+import { client } from '@/lib/prismic';
+import Prismic from 'prismic-javascript'
+import PrismicDom from 'prismic-dom'
+import { Document } from 'prismic-javascript/types/documents'
+import Link from 'next/link'
 
 interface ICategoryProps {
-    products: IProduct[];
+    category: Document;
+    products: Document[];
 }
 
-export default function Category({ products }: ICategoryProps) {
+export default function Category({ category, products }: ICategoryProps) {
     const router = useRouter();
 
-    if(router.isFallback){
+
+    console.log(products)
+    if (router.isFallback) {
         return <p>Carregando...</p>
     }
 
     return (
         <div>
-            <h1>{router.query.slug}</h1>
+            <h1> {PrismicDom.RichText.asText(category.data.title)}</h1>
 
             <ul>
-                {products.map(data => {
+                {products.map(product => {
                     return (
-                        <li key={data.id}>{data.title}</li>
+                        <li key={product.id}>
+                            <Link href={`/catalog/products/${product.uid}`}>
+                                <a>
+                                    {PrismicDom.RichText.asText(product.data.title)}
+                                </a>
+                            </Link>
+                        </li>
                     )
                 })}
             </ul>
@@ -34,12 +43,13 @@ export default function Category({ products }: ICategoryProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const response = await fetch(`http://localhost:3333/categories`);
-    const categories = await response.json();
+    const categories = await client().query([
+        Prismic.Predicates.at('document.type', 'category')
+    ])
 
-    const paths = categories.map(category => {
+    const paths = categories.results.map(category => {
         return {
-            params: { slug: category.id }
+            params: { slug: category.uid }
         }
     })
 
@@ -52,12 +62,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<ICategoryProps> = async (context) => {
     const { slug } = context.params;
 
-    const response = await fetch(`http://localhost:3333/products?category_id=${slug}`);
-    const products = await response.json();
+    const category = await client().getByUID('category', String(slug), {})
+
+    const products = await client().query([
+        Prismic.Predicates.at('document.type', 'product'),
+        Prismic.Predicates.at('my.product.category', category.id)
+
+    ])
+
+   
 
     return {
         props: {
-            products
+            category,
+            products: products.results
         },
         revalidate: 60,
     }
--- first-project-next/src/pages/catalog/products/[slug].tsx
+++ first-project-next/src/pages/catalog/products/[slug].tsx
@@ -1,28 +1,53 @@
 import { useRouter } from 'next/router'
-import dynamic from 'next/dynamic'
-import { useState } from 'react';
+import { GetStaticPaths, GetStaticProps } from "next"
+import { client } from '@/lib/prismic';
+import Prismic from 'prismic-javascript'
+import PrismicDom from 'prismic-dom'
+import { Document } from 'prismic-javascript/types/documents'
 
-const AddToCartModal = dynamic (
-    () => import('@/components/AddToCartModal'), 
-    {loading: () => <p>Loading...</p>, ssr: false}
-);
+interface IProductProps {
+    product: Document;
+}
 
-export default function Product() {
+export default function Product({ product }: IProductProps) {
     const router = useRouter();
-    const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-    function handleAddToCart() {
-        setIsAddToCartModalVisible(true)
+    if(router.isFallback){
+        return <p>Loading...</p>
     }
+
+    console.log(product.data)
 
     return (
         <div>
-            <h1>{router.query.slug}</h1>
+            <h1>{PrismicDom.RichText.asText(product.data.title)}</h1>
 
+            <img src={product.data.thumbnail.url} width="300" alt=""/>
 
-            <button onClick={handleAddToCart}>Add to cart</button>
-
-            {isAddToCartModalVisible && <AddToCartModal/>}
+            <div dangerouslySetInnerHTML={{__html: PrismicDom.RichText.asHtml(product.data.description)}}></div>
+            <p>Price: ${product.data.price}</p>
+        
         </div>
         )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+
+    return {
+        paths: [],
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps<IProductProps> = async (context) => {
+    const { slug } = context.params;
+
+    const product = await client().getByUID('product', String(slug), {})
+    
+    return {
+        props: {
+            product
+        },
+        revalidate: 5,
+    }
 }
